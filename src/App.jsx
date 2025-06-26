2240114cbf31cf2ddf0661e29cdb6e759095421c
thnkthuhigh <a01204496068@gmail.com>
2025-06-26 18:39:54 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500">
+      <h1 className="text-white text-4xl font-bold">Hello React + Vite + Tailwind!</h1>
+    </div>
+  );
+}
+export default App;
